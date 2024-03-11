f8d760a83915b762d3a9bd2d888121a7f16bf298
root <wachirajob@gmail.com>
2024-03-11 17:56:00 +0300

    Added -b flag to 101, removed extra newline from 100

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,7 +2,7 @@
 
 import sys
 
-str = f'and that piece of art is useful - Dora Korpar, 2015-10-19\n'
+str = f'and that piece of art is useful - Dora Korpar, 2015-10-19'
 
 sys.stderr.write(str)
 sys.exit(1)
