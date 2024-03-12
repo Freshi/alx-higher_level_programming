6d9c172c3be70a09ef79c5f075208d9c05bf6e3f
root <wachirajob@gmail.com>
2024-03-12 16:15:58 +0300

    More corrections, linting stuff

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for c in str:
         if ord(c) >= 65:
-	    print("{}".format(chr(ord(c) - 32)), end='')
+            print("{}".format(chr(ord(c) - 32)), end='')
         else:
-	    print(chr(ord(c)), end="")
+            print(chr(ord(c)), end="")
