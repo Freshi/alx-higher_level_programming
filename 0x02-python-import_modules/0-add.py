a282e77f70e562fbbb1dcbef13bba78cc999da86
root <wachirajob@gmail.com>
2024-03-14 12:45:41 +0300

    Added add_o for tests

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    from add_o import add
-    b = 2
-    a = 1
+    from add_0 import add
+    a = 2
+    b = 1
     print('{} + {} + {}'.format(a, b, add(a, b)))
