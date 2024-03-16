606c35b416dda88a014b1d05163adc856381f115
root <wachirajob@gmail.com>
2024-03-16 21:45:17 +0300

    Added int falgs to tasks 2 - 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix = [[]]):
     for l in matrix:
         for item in l:
-            print("{}".format(item), sep = ' ', end='')
+            print("{:d}".format(item), sep = ' ', end='')
         print('')
