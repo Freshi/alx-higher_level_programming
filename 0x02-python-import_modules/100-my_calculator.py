43177e446685f66058e5cd9339328a46e7592f80
root <wachirajob@gmail.com>
2024-03-20 19:14:15 +0300

    Correctedreturned strings on task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-mycalculator.py <a> <operator> <b>")
+        print("./100-mycalculator.py <a> <operator> <b>")
         sys.exit(1)
     if sys.argv[2] not in '+-*/':
-        print("Unknown Operator. Available operators: +,-,* and /")
+        print("Unknown Operator. Only:+,-,*and/ available")
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
