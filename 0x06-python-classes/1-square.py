e36df1db2dded6fbb65d87222288ed70bb8a75c9
root <wachirajob@gmail.com>
2024-04-03 05:20:13 +0300

    Added tasks 0 and 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    '''Initilises a square of size
+
+    Attributes:
+        size (int): side of one side of the square
+
+    '''
+    def __init__(self, size):
+        self.__size = size
