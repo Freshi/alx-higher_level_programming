5ffea4745f48766e32d8557f2b5b5725a8dfc332
root <wachirajob@gmail.com>
2024-04-28 12:43:55 +0300

    Added tasks 4 & 5

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Square class that contains a private size attribute
+that has a setter and a getter to retrieve it, done in
+the most elegant and pythonic way
+"""
+
+
+class Square:
+    """
+    Initialises a square of size s
+
+    Attributes: s(int) - side of square
+    """
+
+    def __init__(self, size=0):
+        """Initialises instance varaibles if provided or with defaults
+
+        Args:
+
+            size(int): Side of square, default: 0, optional
+
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.size ** 2
