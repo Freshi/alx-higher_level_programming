8a28450e1082390ebf1bb228ba04cb6e48e3209f
root <wachirajob@gmail.com>
2024-06-13 12:00:08 +0300

    Added tasks 7-9

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+'''
+Rectangle
+    -Initialises a class rectangle with a width and height.
+'''
+
+
+class Rectangle:
+    '''
+    Initialises a rectangle object
+        width - int, width of rectangle, not negtive
+        height - int, height of rectangle, not negative
+    '''
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return sum((self.__height, self.__width)) * 2
+
+    def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return ''
+        rectangle = []
+        for line in range(self.__height):
+            [rectangle.append(str(self.print_symbol))
+                for _ in range(self.__width)]
+            if line != self.__height - 1:
+                rectangle.append('\n')
+        return ''.join(rectangle)
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        type(self).number_of_instances -= 1
+        print('Bye rectangle...')
