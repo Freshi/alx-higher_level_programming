2cf5dfa69556a0b5863dd071fc01e1f78f7a03fd
root <wachirajob@gmail.com>
2024-06-13 13:40:03 +0300

    Added task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A function that adds two integers together.
+"""
+
+
+def add_integer(a, b=98):
+    '''
+    Function that adds two numbers together.
+    
+    Args:
+        a (int, float) - first number
+        b (int, float) - second number, default=98
+    '''
+    if not any((isinstance(a, int), isinstance(a, float))):
+        raise TypeError('a must be an integer')
+    elif not any((isinstance(b, int), isinstance(b, float))):
+        raise TypeError('b must be an integer')
+    else:
+        return int(a) + int(b)
+
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+Introduction to Test Driven Development using python. From now on the goal is to write modules and functions together with their unit tests.
