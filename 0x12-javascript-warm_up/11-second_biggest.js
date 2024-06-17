032f1db18850707ea8c4e7650002db5996a29de0
root <wachirajob@gmail.com>
2024-06-17 16:40:01 +0300

    Added tasks 12 and 13

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ const { argv } = require('node:process');
 if (argv.length <= 3) {
   console.log(0);
 } else {
- let args = argv.map( x => Number(x));
+  const args = argv.map(x => Number(x));
   console.log(args.sort((a, b) => a - b).at(-2));
 }
